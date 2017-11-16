24fd970f78f883e2c5d825333c71e5f779320920
al-yanez <aleyanez.rubio@gmail.com>
2017-11-16 16:06:12 -0500

    Comments were added to the code for better understanding

--- simulate.R
+++ simulate.R
@@ -1,7 +1,12 @@
+# evaluate the performance of estimator of linear model 
+
+#Step 1: Define real parameters values for beta0 and beta1
 x <- 1:20
 beta0 <- 3
 beta1 <- 0.5
 y <- beta0 + beta1*x
+
+#Step 2: Create possible observed Y values (1000 simulated data sets)
 y1 <- beta0 + beta1 * x + rnorm(20, sd=1)
 data.frame(x,y,y1)
 plot(y~x)
@@ -11,10 +16,18 @@ for(i in 1:1000) Y[,i] <- beta0 + beta1 * x + rnorm(20)
 Y[,1:3]
 par(mfrow=c(3,3))
 for(i in 1:9) plot(Y[,i]~x, main=i)
-estimator <- function(y)
+
+#Step 3: Apply estimation model to all 1000 simulated data sets
+estimator <- function(x,y)
 {
   beta0 <- coef(lm(y~x))[[1]]
   beta0
 }
 Beta0 <- numeric(1000)
-for(i in 1:1000) Beta0[i] <- estimator(Y[,i])
+for(i in 1:1000) Beta0[i] <- estimator(x, Y[,i])
+
+#Step 4: Evaluate performance of estimator
+
+hist(Beta0)
+mean(Beta0)
+
